5fbffe734eeae6b8647965daa2f44b034be05744
fqniu <1966798438@qq.com>
2020-09-22 15:51:19 +0800

    cricle

--- app/src/views/circle/Circle.jsx
+++ app/src/views/circle/Circle.jsx
@@ -1,12 +1,23 @@
 import React from "react"
+import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import NavHeader from "@/components/navHeader/NavHeader";
+import '@/assets/circle/circle.scss'
 
-class Circl extends React.Component {
-
-  render() {
-    return (
-      <div>Circl</div>
-    )
+function Circle() {
+  let goto = () => {
+    console.log(1);
   }
+  return (
+    <div className="circle">
+      <NavHeader className="circle_header">
+        <div data-p="header_l" onClick={goto}><span>《</span></div>
+        <div data-p="header_c">圈子</div>
+        <div data-p="header_r">...</div>
+      </NavHeader>
+      <SearchBar placeholder="Search" maxLength={8} className="circle_searchBar"/>
+
+    </div>
+  )
 }
 
-export default Circl;
+export default Circle;
